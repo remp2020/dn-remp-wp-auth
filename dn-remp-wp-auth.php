660d2d8a82539ecc7a2b315ebd17b5e6cbd4a459
Michal Rusina <michalrusina@gmail.com>
2020-03-05 12:26:24 +0100

    initial commit

---
+++ dn-remp-wp-auth.php
@@ -0,0 +1,16 @@
+<?php
+
+
+/**
+ * Plugin Name: DN REMP WP Auth
+ * Plugin URI:  https://dennikn.sk/
+ * Description: Wordpress login, authentification and user data retrieval
+ * Version:     1.0.0
+ * Author:      Michal Rusina
+ * Author URI:  http://michalrusina.sk/
+ * License:     MIT
+ */
+
+if ( !defined( 'WPINC' ) ) {
+	die;
+}
